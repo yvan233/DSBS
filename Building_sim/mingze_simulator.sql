58ea51360cc6dc9432674c70f3bb7b8e96037207
XingTian1994 <84822613+XingTian1994@users.noreply.github.com>
2022-11-29 17:02:43 +0800

    1129

---
+++ Building_sim/mingze_simulator.sql
@@ -0,0 +1,303 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : Local
+Source Server Version : 80031
+Source Host           : localhost:3306
+Source Database       : mingze_simulator
+
+Target Server Type    : MYSQL
+Target Server Version : 80031
+File Encoding         : 65001
+
+Date: 2022-11-29 16:58:04
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for heatpump
+-- ----------------------------
+DROP TABLE IF EXISTS `heatpump`;
+CREATE TABLE `heatpump` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for heatpump_control
+-- ----------------------------
+DROP TABLE IF EXISTS `heatpump_control`;
+CREATE TABLE `heatpump_control` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for pipe_net
+-- ----------------------------
+DROP TABLE IF EXISTS `pipe_net`;
+CREATE TABLE `pipe_net` (
+  `time` int NOT NULL,
+  `node_pressure` varchar(10000) DEFAULT NULL,
+  `branch_flow` varchar(10000) DEFAULT NULL,
+  PRIMARY KEY (`time`)
+) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+
+-- ----------------------------
+-- Table structure for pump1
+-- ----------------------------
+DROP TABLE IF EXISTS `pump1`;
+CREATE TABLE `pump1` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for pump1_control
+-- ----------------------------
+DROP TABLE IF EXISTS `pump1_control`;
+CREATE TABLE `pump1_control` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for pump2
+-- ----------------------------
+DROP TABLE IF EXISTS `pump2`;
+CREATE TABLE `pump2` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for pump2_control
+-- ----------------------------
+DROP TABLE IF EXISTS `pump2_control`;
+CREATE TABLE `pump2_control` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room1
+-- ----------------------------
+DROP TABLE IF EXISTS `room1`;
+CREATE TABLE `room1` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room1_control
+-- ----------------------------
+DROP TABLE IF EXISTS `room1_control`;
+CREATE TABLE `room1_control` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room2
+-- ----------------------------
+DROP TABLE IF EXISTS `room2`;
+CREATE TABLE `room2` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room2_control
+-- ----------------------------
+DROP TABLE IF EXISTS `room2_control`;
+CREATE TABLE `room2_control` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room3
+-- ----------------------------
+DROP TABLE IF EXISTS `room3`;
+CREATE TABLE `room3` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room3_control
+-- ----------------------------
+DROP TABLE IF EXISTS `room3_control`;
+CREATE TABLE `room3_control` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room4
+-- ----------------------------
+DROP TABLE IF EXISTS `room4`;
+CREATE TABLE `room4` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room4_control
+-- ----------------------------
+DROP TABLE IF EXISTS `room4_control`;
+CREATE TABLE `room4_control` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room5
+-- ----------------------------
+DROP TABLE IF EXISTS `room5`;
+CREATE TABLE `room5` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room5_control
+-- ----------------------------
+DROP TABLE IF EXISTS `room5_control`;
+CREATE TABLE `room5_control` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room6
+-- ----------------------------
+DROP TABLE IF EXISTS `room6`;
+CREATE TABLE `room6` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room6_control
+-- ----------------------------
+DROP TABLE IF EXISTS `room6_control`;
+CREATE TABLE `room6_control` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room7
+-- ----------------------------
+DROP TABLE IF EXISTS `room7`;
+CREATE TABLE `room7` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room7_control
+-- ----------------------------
+DROP TABLE IF EXISTS `room7_control`;
+CREATE TABLE `room7_control` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room8
+-- ----------------------------
+DROP TABLE IF EXISTS `room8`;
+CREATE TABLE `room8` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for room8_control
+-- ----------------------------
+DROP TABLE IF EXISTS `room8_control`;
+CREATE TABLE `room8_control` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for sensor_outdoor
+-- ----------------------------
+DROP TABLE IF EXISTS `sensor_outdoor`;
+CREATE TABLE `sensor_outdoor` (
+  `time` int NOT NULL,
+  `id` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET gbk COLLATE gbk_chinese_ci NOT NULL,
+  `value` float DEFAULT NULL,
+  PRIMARY KEY (`time`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
